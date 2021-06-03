64933b8ee16141867471a62833ad3ea57e4b1df2
Fábio Mendes <fabiobends@gmail.com>
2021-06-02 23:27:09 -0300

    Fix styling from buttons, Fix auth middleware

--- utils/auth.tsx
+++ utils/auth.tsx
@@ -1,15 +1,19 @@
-import { ElementType, useEffect } from "react";
 import Cookie from "js-cookie";
 import { useRouter } from "next/router";
+import { ElementType, useEffect } from "react";
 
-export default function Auth (WrappedComponent: ElementType) {
+export default function Auth(WrappedComponent: ElementType) {
   const wrapper = (props: any) => {
-
     const router = useRouter();
+
+    function signOut() {
+      Cookie.remove("token");
+    }
 
     useEffect(() => {
       const token = Cookie.get("token");
-      if (!token) {
+      if (token) {
+        signOut();
         router.replace("/");
       }
     }, []);
