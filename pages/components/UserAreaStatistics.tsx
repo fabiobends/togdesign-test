735dc23a70f9e11aeedd6531a30df78d6563947e
Fábio Mendes <fabiobends@gmail.com>
2021-06-04 00:02:33 -0300

    Add user area statistics component

---
+++ pages/components/UserAreaStatistics.tsx
@@ -0,0 +1,7 @@
+import styles from "../../styles/components/UserAreaStatistics.module.css";
+
+export default function UserAreaStatistics() {
+  return <div className={styles.content}>
+    Statistics
+  </div>;
+}
